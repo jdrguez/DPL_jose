d5e4198dd4ffaf7d188229787d3873df86a8f6df
jdrguez <josedomingo.rguez.rguez@gmail.com>
2024-11-08 15:26:13 +0000

    fix

--- PHP/Tarea 4/insert.php
+++ PHP/Tarea 4/insert.php
@@ -1,9 +1,9 @@
 <?php
+$servername = "sql311.infinityfree.com"; // El servidor somos nosotros es el local
+$username = "if0_36601523"; // El usuario
+$password = "s3Kntre6cYpU"; // La contraseña para acceder al administrador. Esto no es seguro hacerlo pero no pasa nada si es solo una prueba
+$dbname = "if0_36601523_mi_base_datos"; // Nombre de la base de datos
 
-$servername = "localhost";
-$username = "Jose";
-$password = "1234";
-$dbname = "mi_base_de_datos";
 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
